3a9ce3e313702a32166bc521c221021e80eaca8e
Antonio Fernández <6615888+tonofll@users.noreply.github.com>
2023-04-24 02:33:51 +0200

    Progress on API SQLAlchemy models + docker compose healthcheck

--- api/launcher.py
+++ api/launcher.py
@@ -1,2 +1,37 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from src.models import Base, CastMember, Character, Play, Scene, User
+
 if __name__ == "__main__":
     print("Hello World - api")
+
+    DATABASE_URL = "postgresql://postgres:dramatize@db:5432/dramatize"
+
+    engine = create_engine(DATABASE_URL, echo=True)
+    meta = Base.metadata
+    meta.drop_all(bind=engine)
+    meta.create_all(bind=engine)
+
+    with Session(engine) as session:
+        play1 = Play(name="TBOM", description="TBOM", performance_dates=[1, 2, 3])
+        play2 = Play(name="Primos", description="Primos", performance_dates=[1, 2, 3])
+        user1 = User(name="John", age=30)
+        session.add_all([play1, play2, user1])
+        session.flush()
+
+        director1 = CastMember(roles=["DIRECTOR"], user_id=user1.id, play_id=play1.id)
+        actor1 = CastMember(user_id=user1.id, play_id=play2.id)
+        session.add_all([director1, actor1])
+        session.flush()
+
+        ch1 = Character(name="John", play_id=play1.id)
+        ch2 = Character(name="Mary", play_id=play1.id)
+        ch3 = Character(name="Felix", play_id=play1.id)
+        session.add_all([ch1, ch2, ch3])
+        session.flush()
+
+        scene1 = Scene(name="Scene 1", play_id=play1.id, characters=[ch1, ch2])
+        scene2 = Scene(name="Scene 2", play_id=play1.id, characters=[ch2, ch3])
+        session.add_all([scene1, scene2])
+        session.commit()
+        print("#### Created successfully ####")
---
+++ api/src/models.py
@@ -0,0 +1,177 @@
+import enum
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy.dialects.postgresql import ARRAY, ENUM, JSON
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+# from sqlmodel import ARRAY, JSON, Column, Enum, Field, Integer, SQLModel
+
+
+class Base(DeclarativeBase):
+    """."""
+
+    pass
+
+
+# Many-to-many relationship between characters and scenes
+# (e.g. a character can be in multiple scenes and a scene can have multiple characters)
+at_scene_character = Table(
+    "at_scene_character",
+    Base.metadata,
+    Column("scene_id", Integer, ForeignKey("scenes.id", ondelete="CASCADE")),
+    Column("character_id", Integer, ForeignKey("characters.id", ondelete="CASCADE")),
+)
+
+# Many-to-many relationship between characters and cast members
+# (e.g. a character can be played by multiple cast members and a cast member can play
+#  multiple characters)
+at_cast_member_character = Table(
+    "at_castmember_character",
+    Base.metadata,
+    Column("cast_member_id", Integer, ForeignKey("cast_members.id", ondelete="CASCADE")),
+    Column("character_id", Integer, ForeignKey("characters.id", ondelete="CASCADE")),
+)
+
+
+class User(Base):
+    """."""
+
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True, autoincrement=True)  # noqa: A003
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    age: Mapped[int | None] = mapped_column(Integer)
+    cast_members: Mapped[list["CastMember"]] = relationship("CastMember", back_populates="user")
+
+
+class CastRole(enum.Enum):
+    """."""
+
+    DIRECTOR = enum.auto()
+    ACTOR = enum.auto()
+
+
+class CastMember(Base):
+    """."""
+
+    __tablename__ = "cast_members"
+
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True, autoincrement=True)  # noqa: A003
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"), primary_key=True, autoincrement=False
+    )
+    play_id: Mapped[int] = mapped_column(
+        ForeignKey("plays.id"), primary_key=True, autoincrement=False
+    )
+    availability: Mapped[JSON] = mapped_column(JSON, default={})
+    roles: Mapped[ARRAY] = mapped_column(ARRAY(ENUM(CastRole)), default=[])
+    characters: Mapped[list["Character"]] = relationship(
+        "Character",
+        secondary=at_cast_member_character,
+        back_populates="cast_members",
+        passive_deletes=True,
+    )
+    user: Mapped[User] = relationship("User", back_populates="cast_members", passive_deletes=True)
+
+    # __table_args__ = (PrimaryKeyConstraint("play_id", "user_id"),)
+
+
+class Play(Base):
+    """."""
+
+    __tablename__ = "plays"
+
+    id: Mapped[int | None] = mapped_column(  # noqa: A003
+        primary_key=True, unique=True, nullable=False, autoincrement=True
+    )
+    name: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str] = mapped_column(String(255))
+    original_author_name: Mapped[str | None] = mapped_column(String(255))
+    genre: Mapped[str | None] = mapped_column(String(255))
+    year: Mapped[int | None] = mapped_column(Integer)
+    performance_dates: Mapped[list[int] | None] = mapped_column(ARRAY(Integer))
+    estimated_duration_sec: Mapped[int | None] = mapped_column(Integer)
+
+
+class Scene(Base):
+    """."""
+
+    __tablename__ = "scenes"
+
+    id: Mapped[int | None] = mapped_column(  # noqa: A003
+        primary_key=True, unique=True, nullable=False, autoincrement=True
+    )
+    name: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str | None] = mapped_column(String(255))
+    play_id: Mapped[int] = mapped_column(ForeignKey("plays.id"))
+    characters: Mapped[list["Character"]] = relationship(
+        "Character",
+        secondary=at_scene_character,
+        back_populates="scenes",
+        passive_deletes=True,
+    )
+
+
+class Character(Base):
+    """."""
+
+    __tablename__ = "characters"
+
+    id: Mapped[int | None] = mapped_column(  # noqa: A003
+        primary_key=True, unique=True, nullable=False, autoincrement=True
+    )
+    name: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str | None] = mapped_column(String(255))
+    play_id: Mapped[int] = mapped_column(ForeignKey("plays.id"))
+
+    scenes: Mapped[list["Scene"]] = relationship(
+        "Scene",
+        secondary=at_scene_character,
+        back_populates="characters",
+        passive_deletes=True,
+    )
+    cast_members: Mapped[list["CastMember"]] = relationship(
+        "CastMember",
+        secondary=at_cast_member_character,
+        back_populates="characters",
+        passive_deletes=True,
+    )
+
+
+"""
+class Character(SQLModel, table=True):
+
+    __tablename__ = "characters"
+
+    id: int | None = Field(default=None, primary_key=True)  # noqa: A003
+    name: str
+    description: str
+    play_id: int
+    actors_ids: list[int] = []  # multiple actors can play the same character
+
+
+class Scene(SQLModel, table=True):
+
+    __tablename__ = "scenes"
+
+    id: int | None = Field(default=None, primary_key=True)  # noqa: A003
+    name: str
+    description: str
+    play_id: int
+    character_ids: list[int] = []  # multiple characters can be in the same scene
+
+
+class Rehearsal(SQLModel, table=True):
+
+    __tablename__ = "rehearsals"
+
+    id: int | None = Field(default=None, primary_key=True)  # noqa: A003
+    name: str
+    description: str
+    play_id: int
+    scene_ids: list[int] = []  # multiple scenes can be in the same rehearsal
+    time_start: int
+    time_end: int
+    location: str
+
+"""
